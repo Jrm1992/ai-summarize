fe8cd3db12cb7bc70e7def21f1d9f0eeb5e68958
Jrm1992 <jose_roberto_6@msn.com>
2023-10-15 18:18:13 -0300

    feat(hero): created Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,28 @@
+import { logo } from '../assets'
+
+
 export default function Hero() {
   return (
-    <div className="hero">
-      Hero
-    </div>
+    <header className="w-full flex justify-center items-center flex-col">
+    <nav className="flex justify-between items-center w-full mb-10 pt-3">
+      <img src={logo} alt="logo" className="w-28 object-contain" />
+
+      <button 
+        type='button' 
+        onClick={() => window.open('https://github.com/abhishek-rajput/ai-summarize')} 
+        className="black_btn"
+        >
+          GitHub
+      </button>
+
+    </nav>
+    <h1 className="head_text">
+      Summarize Articles with <br className="max-md:hidden"  />
+      <span className="orange_gradient">OpenAI GPT-4</span>
+    </h1>
+    <h2 className='desc'>
+      Simplify your reading with Summize, an open-source article summarizer that transforms long articles into clear and concise summaries
+    </h2>
+  </header>
   )
 }
