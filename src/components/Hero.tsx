cce1dfda9cc94da1a251035c8b4c11e08e9b2b35
Jrm1992 <jose_roberto_6@msn.com>
2023-10-15 22:16:52 -0300

    feat(hero): updated hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,10 @@ export default function Hero() {
   return (
     <header className="w-full flex justify-center items-center flex-col">
     <nav className="flex justify-between items-center w-full mb-10 pt-3">
-      <img src={logo} alt="logo" className="w-28 object-contain" />
+      <div className='flex justify-center items-center'>
+        <img src={logo} alt="logo" className="w-24 object-contain" />
+        <span className='font-bold text-2xl'>Ai-Summarize</span>
+      </div>
 
       <button 
         type='button' 
@@ -18,7 +21,7 @@ export default function Hero() {
     </nav>
     <h1 className="head_text">
       Summarize Articles with <br className="max-md:hidden"  />
-      <span className="orange_gradient">OpenAI GPT-4</span>
+      <span className="blue_gradient">OpenAI GPT-4</span>
     </h1>
     <h2 className='desc'>
       Simplify your reading with Summize, an open-source article summarizer that transforms long articles into clear and concise summaries
